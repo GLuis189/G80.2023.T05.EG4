7d3cd297d58277f12ac8a8b1a9f6b4d45a59c14c
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-04-20 13:58:32 +0200

    deshacer el cambio de ex

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -63,8 +63,8 @@ class OrderManager:
         except FileNotFoundError:
             # file is not found , so  init my data_list
             data_list = []
-        except json.JSONDecodeError as exception:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from exception
+        except json.JSONDecodeError as ex:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
 
         found = False
         for item in data_list:
@@ -77,8 +77,8 @@ class OrderManager:
         try:
             with open(file_store, "w", encoding="utf-8", newline="") as file:
                 json.dump(data_list, file, indent=2)
-        except FileNotFoundError as exception:
-            raise OrderManagementException("Wrong file or file path") from exception
+        except FileNotFoundError as ex:
+            raise OrderManagementException("Wrong file or file path") from ex
         return True
 
     @staticmethod
@@ -102,8 +102,8 @@ class OrderManager:
         except FileNotFoundError:
             # file is not found , so  init my data_list
             data_list = []
-        except json.JSONDecodeError as exception:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from exception
+        except json.JSONDecodeError as ex:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
 
         #append the shipments list
         data_list.append(shipment.__dict__)
@@ -111,8 +111,8 @@ class OrderManager:
         try:
             with open(shimpents_store_file, "w", encoding="utf-8", newline="") as file:
                 json.dump(data_list, file, indent=2)
-        except FileNotFoundError as exception:
-            raise OrderManagementException("Wrong file or file path") from exception
+        except FileNotFoundError as ex:
+            raise OrderManagementException("Wrong file or file path") from ex
 
 
     #pylint: disable=too-many-arguments
