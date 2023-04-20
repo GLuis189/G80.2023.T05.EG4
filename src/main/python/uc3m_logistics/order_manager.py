5594b9697c0b31f74065ba914855d95f6722861c
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-04-20 11:46:02 +0200

    cambio nombre variables en save_store

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -63,8 +63,8 @@ class OrderManager:
         except FileNotFoundError:
             # file is not found , so  init my data_list
             data_list = []
-        except json.JSONDecodeError as ex:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        except json.JSONDecodeError as exception:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from exception
 
         found = False
         for item in data_list:
@@ -77,8 +77,8 @@ class OrderManager:
         try:
             with open(file_store, "w", encoding="utf-8", newline="") as file:
                 json.dump(data_list, file, indent=2)
-        except FileNotFoundError as ex:
-            raise OrderManagementException("Wrong file or file path") from ex
+        except FileNotFoundError as exception:
+            raise OrderManagementException("Wrong file or file path") from exception
         return True
 
     @staticmethod
