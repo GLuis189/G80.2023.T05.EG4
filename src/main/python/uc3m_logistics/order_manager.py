90f2e4ccd0720a2dc9f0c7d323ce6f376c0c9018
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-04-20 11:44:28 +0200

    cambio nombre variables en save_orders_shipped

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -102,8 +102,8 @@ class OrderManager:
         except FileNotFoundError:
             # file is not found , so  init my data_list
             data_list = []
-        except json.JSONDecodeError as ex:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        except json.JSONDecodeError as exception:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from exception
 
         #append the shipments list
         data_list.append(shipment.__dict__)
@@ -111,8 +111,8 @@ class OrderManager:
         try:
             with open(shimpents_store_file, "w", encoding="utf-8", newline="") as file:
                 json.dump(data_list, file, indent=2)
-        except FileNotFoundError as ex:
-            raise OrderManagementException("Wrong file or file path") from ex
+        except FileNotFoundError as exception:
+            raise OrderManagementException("Wrong file or file path") from exception
 
 
     #pylint: disable=too-many-arguments
