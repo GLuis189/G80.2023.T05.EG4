8fa4c82c0dee836fdcae605a55fdecc533c04783
annasa2003 <95370143+annasa2003@users.noreply.github.com>
2023-04-21 11:49:39 +0200

    llevamos método save_delivere_store a json_store

--- src/main/python/uc3m_logistics/json_store.py
+++ src/main/python/uc3m_logistics/json_store.py
@@ -88,3 +88,23 @@ class JsonStore():
         delivery_date = datetime.fromtimestamp(del_timestamp).date()
         if delivery_date != today:
             raise OrderManagementException("Today is not the delivery date")
+
+    def save_delivere_store(self, tracking_code):
+        shipments_file = JSON_FILES_PATH + "shipments_delivered.json"
+        try:
+            with open(shipments_file, "r", encoding="utf-8", newline="") as file:
+                data_list = json.load(file)
+        except FileNotFoundError as ex:
+            # file is not found , so  init my data_list
+            data_list = []
+        except json.JSONDecodeError as ex:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+
+            # append the delivery info
+        data_list.append(str(tracking_code))
+        data_list.append(str(datetime.utcnow()))
+        try:
+            with open(shipments_file, "w", encoding="utf-8", newline="") as file:
+                json.dump(data_list, file, indent=2)
+        except FileNotFoundError as ex:
+            raise OrderManagementException("Wrong file or file path") from ex
