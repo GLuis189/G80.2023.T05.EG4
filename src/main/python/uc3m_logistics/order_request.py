f6f518dd9d8c8bc1addc91e985e3495a1ff1a498
annasa2003 <95370143+annasa2003@users.noreply.github.com>
2023-04-21 07:02:03 +0200

    Attribute ProductId

--- src/main/python/uc3m_logistics/order_request.py
+++ src/main/python/uc3m_logistics/order_request.py
@@ -8,6 +8,7 @@ from .attribute_phone_number import PhoneNumber
 from .attribute_address import Address
 from .attribute_order_type import OrderType
 from .attribute_zip_code import ZipCode
+from .attribute_product_id import ProductId
 
 
 class OrderRequest:
@@ -15,7 +16,7 @@ class OrderRequest:
     #pylint: disable=too-many-arguments
     def __init__( self, product_id, order_type,
                   delivery_address, phone_number, zip_code ):
-        self.__product_id = self.validate_ean13(product_id)
+        self.__product_id = ProductId(product_id).value
         self.__delivery_address = Address(delivery_address).value
         self.__order_type = OrderType(order_type).value
         self.__phone_number = PhoneNumber(phone_number).value
@@ -76,34 +77,6 @@ class OrderRequest:
         """Returns the order's zip_code"""
         return self.__zip_code
 
-    def validate_ean13( self, ean13:str )->str:
-        """method vor validating a ean13 code"""
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE EAN13
-        # RETURN TRUE IF THE EAN13 IS RIGHT, OR FALSE IN OTHER CASE
-        checksum = 0
-        code_read = -1
-        result = False
-        regex_ean13 = re.compile("^[0-9]{13}$")
-        valid_ean13_format = regex_ean13.fullmatch(ean13)
-        if valid_ean13_format is None:
-            raise OrderManagementException("Invalid EAN13 code string")
-
-        for i, digit in enumerate(reversed(ean13)):
-            try:
-                current_digit = int(digit)
-            except ValueError as value_error:
-                raise OrderManagementException("Invalid EAN13 code string") from value_error
-            if i == 0:
-                code_read = current_digit
-            else:
-                checksum += (current_digit) * 3 if (i % 2 != 0) else current_digit
-        control_digit = (10 - (checksum % 10)) % 10
-
-        if (code_read != -1) and (code_read == control_digit):
-            result = True
-        else:
-            raise OrderManagementException("Invalid EAN13 control digit")
-        return ean13
 
 
 
