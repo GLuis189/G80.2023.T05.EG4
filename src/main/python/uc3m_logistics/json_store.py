715d94253a69e2d13558b3bd261f6174a995205a
annasa2003 <95370143+annasa2003@users.noreply.github.com>
2023-04-21 12:04:21 +0200

    extraemos metodo load_store

--- src/main/python/uc3m_logistics/json_store.py
+++ src/main/python/uc3m_logistics/json_store.py
@@ -8,19 +8,11 @@ class JsonStore():
     def __init__(self):
         pass
 
-    @staticmethod
-    def save_store(data: OrderRequest) -> True:
+    def save_store(self, data: OrderRequest) -> True:
         """Medthod for saving the orders store"""
         file_store = JSON_FILES_PATH + "orders_store.json"
         # first read the file
-        try:
-            with open(file_store, "r", encoding="utf-8", newline="") as file:
-                data_list = json.load(file)
-        except FileNotFoundError:
-            # file is not found , so  init my data_list
-            data_list = []
-        except json.JSONDecodeError as ex:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        data_list = self.load_store(file_store)
 
         found = False
         for item in data_list:
@@ -37,19 +29,23 @@ class JsonStore():
             raise OrderManagementException("Wrong file or file path") from ex
         return True
 
-    @staticmethod
-    def save_orders_shipped(shipment: dict)->None:
-        """Saves the shipping object into a file"""
-        shimpents_store_file = JSON_FILES_PATH + "shipments_store.json"
-        # first read the file
+
+    def load_store(self, file_store):
         try:
-            with open(shimpents_store_file, "r", encoding="utf-8", newline="") as file:
+            with open(file_store, "r", encoding="utf-8", newline="") as file:
                 data_list = json.load(file)
         except FileNotFoundError:
             # file is not found , so  init my data_list
             data_list = []
         except json.JSONDecodeError as ex:
             raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        return data_list
+
+    def save_orders_shipped(self, shipment: dict)->None:
+        """Saves the shipping object into a file"""
+        shimpents_store_file = JSON_FILES_PATH + "shipments_store.json"
+        # first read the file
+        data_list = self.load_store(shimpents_store_file)
 
         #append the shipments list
         data_list.append(shipment.__dict__)
