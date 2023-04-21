e881fb4a054af044cac2c210686fc59143a20ea8
annasa2003 <95370143+annasa2003@users.noreply.github.com>
2023-04-21 11:19:46 +0200

    extraemos método read de deliver product

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -86,15 +86,7 @@ class OrderManager:
         self.validate_tracking_code(tracking_code)
 
         #check if this tracking_code is in shipments_store
-        shimpents_store_file = JSON_FILES_PATH + "shipments_store.json"
-        # first read the file
-        try:
-            with open(shimpents_store_file, "r", encoding="utf-8", newline="") as file:
-                data_list = json.load(file)
-        except json.JSONDecodeError as ex:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
-        except FileNotFoundError as ex:
-            raise OrderManagementException("shipments_store not found") from ex
+        data_list = self.read_shipping_store()
         #search this tracking_code
         found = False
         for item in data_list:
@@ -129,3 +121,15 @@ class OrderManager:
         except FileNotFoundError as ex:
             raise OrderManagementException("Wrong file or file path") from ex
         return True
+
+    def read_shipping_store(self):
+        shimpents_store_file = JSON_FILES_PATH + "shipments_store.json"
+        # first read the file
+        try:
+            with open(shimpents_store_file, "r", encoding="utf-8", newline="") as file:
+                data_list = json.load(file)
+        except json.JSONDecodeError as ex:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        except FileNotFoundError as ex:
+            raise OrderManagementException("shipments_store not found") from ex
+        return data_list
